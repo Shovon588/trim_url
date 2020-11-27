10593d9120a5cb833b89c45c8be4dfb43298ab22
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-11-27 23:29:30 +0600

    Removing django project files

--- choto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . views import home, handle_url
-
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('<str:uuid>', handle_url, name="handle_url"),
-]
